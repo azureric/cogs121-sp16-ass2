caf6048f8a877fd9ee14d4aec9c766122f65274f
Lok Wong <lywong@ucsd.edu>
2016-04-25 16:04:14 -0700

    sample works

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/delphidata', function (req, res) {
             if(err) {
             return console.error('error running query', err);
             }
-            //console.log(result.rows);
+            console.log(result.rows);
             /* change this */
             // console.log(result.rows[0].value);
             //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
